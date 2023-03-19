af2d39a3de59f7c15f21b0b759bf7d635f0266ca
Akeemandela <easytwenty14@gmail.com>
2023-03-20 00:38:44 +0100

    Updated final files

--- BlogNaijaBE/app.js
+++ BlogNaijaBE/app.js
@@ -1,30 +1,87 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 
 const app = express();
 
 // Connect to MongoDB..... uncomment to connect to the Database
-// const dbURI = 'mongodb+srv://swift:Magnetic123@alx-portfolio-project.fkxjfut.mongodb.net/alx-portfolio-project?retryWrites=true&w=majority'
-// mongoose.connect(dbURI);
+const dbURI = 'mongodb+srv://swift:Magnetic123@alx-project.fkxjfut.mongodb.net/alx-portfolio-project?retryWrites=true&w=majority'
+
+
+mongoose.connect(dbURI)
+    .then( () => app.listen(3000))
+    
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-// Listen for request
-app.listen(3000);
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home Page</p>')
-    res.render('index', { title: 'Home'});
+    res.redirect('/blogs');
+    
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>Home Page</p>')
     res.render('about', { title: 'About Page'});
 });
+
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort( { createdAt: -1})
+      .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result})
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+      .then((result) => {
+        res.redirect('/blogs');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+})
+
+app.get('/blogs/:id', (req, res) =>{
+    const id = req.params.id;
+    Blog.findById(id)
+      .then((result) => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+     
+       
+})
+
+
+app.delete('/blogs/:id', (req, res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+      .then((result) => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+     
+       
+})
+
 
 app.get('/blogs/create', (req, res) => {
     // res.send('<p>Home Page</p>')
